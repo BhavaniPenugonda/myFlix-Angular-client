a986bec74ae9424af63817ef6290fc5973d369f4
BhavaniPenugonda <bhavani0202@gmail.com>
2025-01-23 15:56:29 +0100

    import is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatDialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
