58a29983ff437683d840c1d6ca49a6dc8cbaeb02
BhavaniPenugonda <bhavani0202@gmail.com>
2025-01-23 14:29:56 +0100

    dependencies are imported and constructor is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-root',
@@ -9,4 +13,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+// This is the function that will open the dialog when the signup button is clicked  
+openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+// Assigning the dialog a width
+    width: '280px'
+    });
+  }
 }
