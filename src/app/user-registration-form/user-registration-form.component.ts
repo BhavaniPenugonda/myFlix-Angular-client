42676ba58605d5872d12a28595f243a91e24d0b6
BhavaniPenugonda <bhavani0202@gmail.com>
2025-01-24 15:13:44 +0100

    userregistration function is updated

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -39,20 +39,21 @@ export class UserRegistrationFormComponent implements
   
   // This is the function responsible for sending the form inputs to the backend
   registerUser(): void {
-      this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
-    // Logic for a successful user registration goes here! (To be implemented)
-       this.dialogRef.close(); // This will close the modal on success!
-       this.snackBar.open(result, 'OK', {
-          duration: 2000
-       });
-      }, (result) => {
-        this.snackBar.open(result, 'OK', {
-          duration: 2000
-        });
-      });
-    }
-  
-  }
+      this.fetchApiData.userRegistration(this.userData).subscribe({
+        next: (result) => {
+          this.dialogRef.close();
+          this.snackBar.open("Registration success",  "OK", {
+              duration: 2000
+          });
+      },
+      error: (err) => {
+          this.snackBar.open("Registration failed", "OK", {
+              duration: 2000
+          });
+      }
+  });
+}
+}
 
    
 
