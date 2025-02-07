120c012703940cdd5664db0adf16fb6f2201b526
BhavaniPenugonda <bhavani0202@gmail.com>
2025-02-07 12:48:45 +0100

    genredialog is implemented

--- src/app/genre-dialog/genre-dialog.component.ts
+++ src/app/genre-dialog/genre-dialog.component.ts
@@ -1,9 +1,10 @@
 
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-
+import { MatDialogModule } from '@angular/material/dialog';
 @Component({
   selector: 'app-genre-dialog',
+  imports: [ MatDialogModule,],
   templateUrl: './genre-dialog.component.html',
   styleUrls: ['./genre-dialog.component.scss']
 })
